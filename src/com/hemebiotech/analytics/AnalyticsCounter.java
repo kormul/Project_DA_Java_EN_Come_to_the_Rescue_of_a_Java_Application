a7cdfa2534b0b6e6c1b704fd6992d1c7ba4d477b
kormul <63836128+kormul@users.noreply.github.com>
2021-07-15 09:02:44 +0200

    Update AnalyticsCounter.java
    Fin des correction

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,7 +3,7 @@ package com.hemebiotech.analytics;
 import java.util.ArrayList;
 
 import com.hemebiotech.analytics.io.Reader;
-import com.hemebiotech.analytics.io.Writter;
+import com.hemebiotech.analytics.io.Writer;
 
 public class AnalyticsCounter {
 	
@@ -14,7 +14,7 @@ public class AnalyticsCounter {
 		ArrayList<Symptom> symptoms = new Reader("symptoms.txt").readSymptomDataFromFile();
 		
 		//Writes the symptoms to an output file to record
-		new Writter("result.out").writeSymptomDataFromFile(symptoms);
+		new Writer("result.out").writeSymptomDataFromFile(symptoms);
 		
 	}
 }
