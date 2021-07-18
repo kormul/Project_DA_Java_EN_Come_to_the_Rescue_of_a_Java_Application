a89b2cb1dd440a10a271ce9d42aafbbab6088ce5
Guillaume Aubert <8095025-Kormul@users.noreply.gitlab.com>
2021-07-18 13:46:58 +0200

    Update Utils.java
    Add the javadoc

--- src/com/hemebiotech/analytics/utils/Utils.java
+++ src/com/hemebiotech/analytics/utils/Utils.java
@@ -8,10 +8,13 @@ import com.hemebiotech.analytics.Symptom;
 
 public class Utils {
 	
+	/**
+	 * sorted alphabetically a list of symptom 
+	 * @param list of Symptoms
+	 */
 	public static void sortArrayListSymptoms(ArrayList<Symptom> list) {
 		
 		Collections.sort(list, new Comparator<Symptom>() {
-
 			@Override
 			public int compare(Symptom arg0, Symptom arg1) {
 				return arg0.getName().compareTo(arg1.getName());
